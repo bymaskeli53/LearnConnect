e191ff31d19ad7e58ab6c7d8f6519a13181fba38
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2024-11-28 16:40:47 +0300

    Add turkish support

--- app/src/main/java/com/gundogar/learnconnect/CoursesFragment.kt
+++ app/src/main/java/com/gundogar/learnconnect/CoursesFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -38,6 +39,8 @@ class CoursesFragment : BaseFragment<FragmentCoursesBinding>() {
             viewModel.courses.collect { courses ->
                adapter = CoursesAdapter(courses, itemClickListener = {
                    purchaseCourse(it)
+                   Toast.makeText(requireContext(),
+                       getString(R.string.you_bought_the_course),Toast.LENGTH_SHORT).show()
 
                })
                 binding.rvCourses.adapter = adapter
